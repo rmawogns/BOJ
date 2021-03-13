e0e799352f413682ef82cfacede8a03bf5a15d9b
jaehoonkim <73879034+rmawogns@users.noreply.github.com>
2021-03-13 19:20:13 +0900

    Add files via upload

--- BOJ/Array/3_2577.py
+++ BOJ/Array/3_2577.py
@@ -1,10 +1,9 @@
-l = []
+A = int(input())
+B = int(input())
+C = int(input())
+i = 0
+num = [int(i) for i in str(A * B * C)]
+
 for i in range(10):
-    i = int(input())
-    l.append(i % 42)
-
-print(len(set(l)))
-    
-    
-        
-
+    print(num.count(i))
+    i += 1
