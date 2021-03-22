1e7da1c3041899de0949e9127ad9d316062d07c4
jaehoonkim <73879034+rmawogns@users.noreply.github.com>
2021-03-22 23:17:22 +0900

    Add files via upload

---
+++ BOJ/Sort/6_11650.py
@@ -0,0 +1,12 @@
+n = int(input()) 
+num = [] 
+
+for i in range(n): 
+    [a, b] = map(int, input().split()) 
+    num.append([a, b]) 
+
+num = sorted(num)
+
+for i in range(n): 
+    print(num[i][0], num[i][1])
+
